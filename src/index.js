6bbe312bf3a3b52a7a0a63bf25881f8fa4916e9b
Karthikeyan <imkarthikeyans@gmail.com>
2019-06-07 16:16:59 +0530

    Added the footer and table

--- src/index.js
+++ src/index.js
@@ -11,43 +11,81 @@ function App() {
       <Nav />
       <div className="container" id="section-player">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-5">
             <h4> Learn to code with me</h4>
             <hr />
-            <img
-              src="https://pbcdn1.podbean.com/imglogo/dir-logo/261684/261684_300x300.png"
-              alt="Learn to code"
-            />
-
+            <a href="https://www.podbean.com/podcast-detail/jh5a4-3fe34/Learn-to-Code-With-Me-Podcast">
+              <img
+                src="https://pbcdn1.podbean.com/imglogo/dir-logo/261684/261684_300x300.png"
+                alt="Learn to code"
+                id="LCWTM-img"
+              />
+            </a>
             <hr />
-            {LCWTM.map(data => {
-              return (
-                <div key={data.url}>
-                  <a href={data.url}> {data.text}</a>
-                  <p>Broadcast Date : {data.Broadcast_date}</p>
-                </div>
-              );
-            })}
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Podcast Link </th>
+                  <th scope="col">Broadcast_date</th>
+                </tr>
+              </thead>
+              {LCWTM.map(data => {
+                return (
+                  <tbody key={data.url}>
+                    <tr>
+                      <td>
+                        <a href={data.url}> {data.text.trim()}</a>
+                      </td>
+                      <td>{data.Broadcast_date}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-2" />
+          <div className="col-md-5">
             <h4>JS - Jabber </h4>
             <hr />
-            <img
-              alt="JS-Jabber"
-              src="https://pbcdn1.podbean.com/imglogo/dir-logo/357781/357781_300x300.jpg"
-            />
+            <a href="https://www.podbean.com/podcast-detail/d4un8-57595/JavaScript-Jabber-Podcast">
+              <img
+                alt="JS-Jabber"
+                src="https://pbcdn1.podbean.com/imglogo/dir-logo/357781/357781_300x300.jpg"
+                id="JSJ-img"
+              />
+            </a>
             <hr />
-            {JSjabber.map(data => {
-              return (
-                <div key={data.url}>
-                  <a href={data.url}> {data.text}</a>
-                  <p>Broadcast Date : {data.Broadcast_date}</p>
-                </div>
-              );
-            })}
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Podcast Link </th>
+                  <th scope="col">Broadcast_date</th>
+                </tr>
+              </thead>
+              {JSjabber.map(data => {
+                return (
+                  <tbody key={data.url}>
+                    <tr>
+                      <td>
+                        <a href={data.url}> {data.text.trim()}</a>
+                      </td>
+                      <td>{data.Broadcast_date}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
           </div>
         </div>
       </div>
+      <footer
+        class="footer mt-auto py-3"
+        style={{ backgroundColor: "#f5f5f5" }}
+      >
+        <div class="container text-center">
+          <span class="text-center">Developed by Karthikeyan &hearts; JS </span>
+        </div>
+      </footer>
     </>
   );
 }
--- src/nav.js
+++ src/nav.js
@@ -4,7 +4,9 @@ import "./styles.css";
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <span className="navbar-text mx-auto">Podcast Directory</span>
+      <span className="navbar-text mx-auto" style={{ fontWeight: "bold" }}>
+        <b>Podcast Directory</b>
+      </span>
     </nav>
   );
 };
