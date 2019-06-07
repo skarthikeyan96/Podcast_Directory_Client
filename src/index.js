ac675071a25457ae20f8cf010f473f8767b357a3
Karthikeyan <imkarthikeyans@gmail.com>
2019-06-07 14:30:23 +0530

    Added new source and styles

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import LCWTM from "./source/LCWTM-source";
-
+import JSjabber from "./source/JS-jabber";
+import Nav from "./nav";
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h3> Podcast Directory</h3>
-      {LCWTM.map(data => {
-        return (
-          <div key={data.text}>
-            <a href={data.url}> Link : {data.url}</a>
-            <p>Broadcast Date : {data.text}</p>
+    <>
+      <Nav />
+      <div className="container" id="section-player">
+        <div className="row">
+          <div className="col-md-6">
+            <h4> Learn to code with me</h4>
+            <hr />
+            <img
+              src="https://pbcdn1.podbean.com/imglogo/dir-logo/261684/261684_300x300.png"
+              alt="Learn to code"
+            />
+
+            <hr />
+            {LCWTM.map(data => {
+              return (
+                <div key={data.url}>
+                  <a href={data.url}> {data.text}</a>
+                  <p>Broadcast Date : {data.Broadcast_date}</p>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
+          <div className="col-md-6">
+            <h4>JS - Jabber </h4>
+            <hr />
+            <img
+              alt="JS-Jabber"
+              src="https://pbcdn1.podbean.com/imglogo/dir-logo/357781/357781_300x300.jpg"
+            />
+            <hr />
+            {JSjabber.map(data => {
+              return (
+                <div key={data.url}>
+                  <a href={data.url}> {data.text}</a>
+                  <p>Broadcast Date : {data.Broadcast_date}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-text mx-auto">Podcast Directory</span>
+    </nav>
+  );
+};
+
+export default Nav;
